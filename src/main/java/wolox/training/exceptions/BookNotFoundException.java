cce3135b15b1409aaef169f6190ce634de7719bf
Juan David Bermudez <juandavid.bermudez@wolox.co>
2020-10-09 15:45:42 -0500

    Add book exceptions Add exception handler Add CRUD methods to book controller Fix book find by author method

---
+++ src/main/java/wolox/training/exceptions/BookNotFoundException.java
@@ -0,0 +1,18 @@
+package wolox.training.exceptions;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException() {}
+
+    public BookNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookNotFoundException(final String message) {
+        super(message);
+    }
+
+    public BookNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+}
