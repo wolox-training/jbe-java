a17a55c14c6eed46cb5c84e7766a8582260f28af
Juan David Bermudez <juandavid.bermudez@wolox.co>
2020-10-20 09:54:28 -0500

    Add lombok dependencies Add h2 dependency version Add lombok annotations to User and Book models

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -20,7 +20,6 @@ import com.google.common.base.Strings;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -29,6 +28,11 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -40,38 +44,52 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Book {
 
+    @Getter
+    @Setter
     private String genre;
 
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String author;
 
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String image;
 
+
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String title;
 
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String subtitle;
 
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String publisher;
 
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String year;
 
+    @Getter
     @NotNull
     @Column(nullable = false)
     private String pages;
 
+    @Getter
     @NotNull
     @Column(nullable = false, unique = true)
     private String isbn;
@@ -79,32 +97,15 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOOK_SEQ")
     @SequenceGenerator(name = "BOOK_SEQ", sequenceName = "BOOK_SEQ")
+    @Getter
     private Long id;
 
     @ManyToMany(mappedBy = "books")
     private final List<User> users = new ArrayList<>();
 
-    public Book() {}
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
     public void setAuthor(String author) {
         checkArgument(!Strings.isNullOrEmpty(author), OBLIGATORY_AUTHOR_FIELD);
         this.author = author;
-    }
-
-    public String getImage() {
-        return image;
     }
 
     public void setImage(String image) {
@@ -112,17 +113,9 @@ public class Book {
         this.image = image;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
     public void setTitle(String title) {
         checkArgument(!Strings.isNullOrEmpty(title), OBLIGATORY_TITLE_FIELD);
         this.title = title;
-    }
-
-    public String getSubtitle() {
-        return subtitle;
     }
 
     public void setSubtitle(String subtitle) {
@@ -130,27 +123,15 @@ public class Book {
         this.subtitle = subtitle;
     }
 
-    public String getPublisher() {
-        return publisher;
-    }
-
     public void setPublisher(String publisher) {
         checkArgument(!Strings.isNullOrEmpty(publisher), OBLIGATORY_PUBLISHER_FIELD);
         this.publisher = publisher;
     }
 
-    public String getYear() {
-        return year;
-    }
-
     public void setYear(String year) {
-        checkArgument(!Strings.isNullOrEmpty(year),OBLIGATORY_YEAR_FIELD);
+        checkArgument(!Strings.isNullOrEmpty(year), OBLIGATORY_YEAR_FIELD);
         checkArgument(StringUtils.isNumeric(year), INVALID_YEAR);
         this.year = year;
-    }
-
-    public String getPages() {
-        return pages;
     }
 
     public void setPages(String pages) {
@@ -160,63 +141,13 @@ public class Book {
         this.pages = pages;
     }
 
-    public String getIsbn() {
-        return isbn;
-    }
-
     public void setIsbn(String isbn) {
         checkArgument(!Strings.isNullOrEmpty(isbn), OBLIGATORY_ISBN_FIELD);
         checkArgument(StringUtils.isNumeric(isbn), INVALID_ISBN);
         this.isbn = isbn;
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public List<User> getUsers() {
         return Collections.unmodifiableList(users);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return Objects.equals(genre, book.genre) &&
-            author.equals(book.author) &&
-            image.equals(book.image) &&
-            title.equals(book.title) &&
-            subtitle.equals(book.subtitle) &&
-            publisher.equals(book.publisher) &&
-            year.equals(book.year) &&
-            pages.equals(book.pages) &&
-            isbn.equals(book.isbn) &&
-            id.equals(book.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(genre, author, image, title, subtitle, publisher, year, pages, isbn, id);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-            "genre='" + genre + '\'' +
-            ", author='" + author + '\'' +
-            ", image='" + image + '\'' +
-            ", title='" + title + '\'' +
-            ", subtitle='" + subtitle + '\'' +
-            ", publisher='" + publisher + '\'' +
-            ", year='" + year + '\'' +
-            ", pages='" + pages + '\'' +
-            ", isbn='" + isbn + '\'' +
-            ", id=" + id +
-            '}';
     }
 }
--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -19,7 +19,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -30,6 +29,10 @@ import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 import wolox.training.exceptions.BookNotFoundException;
 
@@ -44,26 +47,33 @@ import wolox.training.exceptions.BookNotFoundException;
 @Table(name = "t_user")
 @ApiModel(description = "Users from Open Library")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
     @SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ")
+    @Getter
     private Long id;
 
     @NotNull
     @Column(nullable = false)
     @ApiModelProperty(notes = "Username to identify the user in the system", required = true)
+    @Getter
     private String username;
 
     @NotNull
     @Column(nullable = false)
     @ApiModelProperty(notes = "User's real name", required = true)
+    @Getter
     private String name;
 
     @NotNull
     @Column(nullable = false)
     @ApiModelProperty(notes = "User's date of birth", required = true)
+    @Getter
     private LocalDate birthdate;
 
     @NotNull
@@ -71,32 +81,14 @@ public class User {
     @ApiModelProperty(notes = "User's books", required = true)
     private List<Book> books = new ArrayList<>();
 
-    public User() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
     public void setUsername(String username) {
         checkArgument(!Strings.isNullOrEmpty(username), OBLIGATORY_USERNAME_FIELD);
         this.username = username;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         checkArgument(!Strings.isNullOrEmpty(name), OBLIGATORY_NAME_FIELD);
         this.name = name;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
     }
 
     public void setBirthdate(LocalDate birthdate) {
@@ -111,8 +103,9 @@ public class User {
 
     /**
      * Add a book to the user's book collection
+     *
      * @param book must not be null
-     * @throws NullPointerException if book is null
+     * @throws NullPointerException      if book is null
      * @throws BookAlreadyOwnedException if user already has the book
      */
     public void addBook(Book book) {
@@ -125,8 +118,9 @@ public class User {
 
     /**
      * Remove a book from the user's book collection
+     *
      * @param book must not be null
-     * @throws NullPointerException if book is null
+     * @throws NullPointerException  if book is null
      * @throws BookNotFoundException if user does not have the book to be removed
      */
     public void removeBook(Book book) {
@@ -135,37 +129,5 @@ public class User {
         } else {
             throw new BookNotFoundException(String.format(BOOK_NOT_FOUND, book.getId()));
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return id.equals(user.id) &&
-            username.equals(user.username) &&
-            name.equals(user.name) &&
-            birthdate.equals(user.birthdate) &&
-            books.equals(user.books);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username, name, birthdate, books);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-            "id=" + id +
-            ", username='" + username + '\'' +
-            ", name='" + name + '\'' +
-            ", birthdate=" + birthdate +
-            ", books=" + books +
-            '}';
     }
 }
