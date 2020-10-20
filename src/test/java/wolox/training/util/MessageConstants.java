a285679ee9662bba22433a78dc69492a615be812
Juan David Bermudez Celedon <juandavid.bermudez@wolox.co>
2020-10-20 09:11:58 -0500

    Fix WRONG_SIZE message

--- src/test/java/wolox/training/util/MessageConstants.java
+++ src/test/java/wolox/training/util/MessageConstants.java
@@ -3,7 +3,7 @@ package wolox.training.util;
 public class MessageConstants {
 
     public final static String EXCEPTION_THROWN = "Expected exception never thrown";
-    public final static String WRONG_SIZE = "Expected exception never thrown";
+    public final static String WRONG_SIZE = "Wrong collection size expected";
     public final static String WRONG_BOOK = "Wrong expected book";
     public final static String WRONG_USER = "Wrong expected user";
 }
